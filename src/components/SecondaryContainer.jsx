10e0173b0715880f6f6f021c560ec826db61ca14
Mohd-Saaad <m.d.saad.@hotmail.com>
2025-12-09 22:51:36 +0530

    missed this

--- src/components/SecondaryContainer.jsx
+++ src/components/SecondaryContainer.jsx
@@ -6,7 +6,7 @@ const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
     <div className="bg-black">
-      <div className="-mt-36 pl-12 relative z-10 ">
+      <div className="-mt-40 pl-12 relative z-10 ">
         <MovieList title={"Now Playing"} movies={movies.nowPlayingMovies} />
         <MovieList title={"Popular"} movies={movies.popularMovies} />
         <MovieList title={"Top Rated"} movies={movies.topMovies} />
